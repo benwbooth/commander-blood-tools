780477be258939451937b5e1482399b8a50a2adb
Ben Booth <benwbooth@gmail.com>
2026-08-15 00:25:11 -0700

    Accept BLOODPRG indexed resource handler

--- re/source/bloodprg/candidates/seg_04da/func_0076ea_index_lookup_1fd7.c
+++ re/source/bloodprg/candidates/seg_04da/func_0076ea_index_lookup_1fd7.c
@@ -4,7 +4,7 @@ const cb_u8 CB_FAR *CB_NEAR index_lookup_1fd7(
     const cb_u8 CB_FAR *script_bytes)
 {
     cb_u16 stored_id;
-    cb_game_char_ptr dst;
+    cb_u16 dst_index;
     cb_u8 ch;
 
     stored_id = (cb_u16)(cb_i16)(cb_i8)*script_bytes++;
@@ -12,16 +12,16 @@ const cb_u8 CB_FAR *CB_NEAR index_lookup_1fd7(
     stored_id = (cb_u16)(0x0dd7u + ((stored_id - 1u) << 4));
     byte_parser_index_word_1fd7 = stored_id;
 
-    dst = byte_parser_index_text_213a;
+    dst_index = 0u;
     for (;;) {
         ch = *script_bytes++;
         if ((cb_i8)ch < 0 || ch < 0x20u) {
             --script_bytes;
             break;
         }
-        *dst++ = (char)ch;
+        byte_parser_index_text_213a[dst_index++] = (char)ch;
     }
-    *dst = '\0';
+    byte_parser_index_text_213a[dst_index] = '\0';
 
     if ((byte_parser_ui_state & 1u) == 0) {
         if (resource_ems_handle != -1) {
