a9d75928f72d825c2e702b842bf7d9e2a7ef314f
Ben Booth <benwbooth@gmail.com>
2026-08-20 22:17:04 -0700

    Use natural C ABI for HUD palette reset

--- re/source/bloodprg/candidates/seg_071e/func_008c96_ship_3d_hud_palette_snapshot_and_camera_reset.c
+++ re/source/bloodprg/candidates/seg_071e/func_008c96_ship_3d_hud_palette_snapshot_and_camera_reset.c
@@ -3,28 +3,7 @@
 
 void CB_FAR ship_3d_hud_palette_snapshot_and_camera_reset(void)
 {
-#if defined(__TURBOC__) || defined(__BORLANDC__)
-    asm push bp;
-    asm push ax;
-    asm push ds;
-    asm push es;
-    asm push di;
-    asm push si;
-    asm push cx;
-    asm push bx;
-    asm push dx;
-#elif defined(__WATCOMC__)
-    /* Watcom saves the allocated general registers but not AX or ES. */
-    _asm push ax;
-    _asm push es;
-#endif
-
     draw_hud_element_2bc7();
-#if defined(__TURBOC__) || defined(__BORLANDC__)
-    asm pushf;
-#elif defined(__WATCOMC__)
-    _asm pushf;
-#endif
 
     _fmemcpy(
         (void CB_FAR *)ship_3d_hud_palette_snapshot,
@@ -34,21 +13,4 @@ void CB_FAR ship_3d_hud_palette_snapshot_and_camera_reset(void)
     ship_3d_camera_x = 10000;
     ship_3d_camera_y = 12000;
     ship_3d_camera_z = 0;
-
-#if defined(__TURBOC__) || defined(__BORLANDC__)
-    asm popf;
-    asm pop dx;
-    asm pop bx;
-    asm pop cx;
-    asm pop si;
-    asm pop di;
-    asm pop es;
-    asm pop ds;
-    asm pop ax;
-    asm pop bp;
-#elif defined(__WATCOMC__)
-    _asm popf;
-    _asm pop es;
-    _asm pop ax;
-#endif
 }
