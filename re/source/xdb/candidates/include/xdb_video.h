1bac8910cfd3b6d02790fe4ef4a073325763cf7e
Ben Booth <benwbooth@gmail.com>
2026-08-21 01:09:04 -0700

    Use natural C for XDB port I/O

--- re/source/xdb/candidates/include/xdb_video.h
+++ re/source/xdb/candidates/include/xdb_video.h
@@ -15,31 +15,23 @@ extern volatile xdb_u16 xdb_video_page_4000; /* DS:0x0026 */
 extern volatile xdb_u16 xdb_video_page_a400; /* DS:0x0028 */
 
 #if defined(__WATCOMC__)
-extern void XDB_NEAR xdb_port_write_u8(xdb_u16 port, xdb_u8 value);
-#pragma aux xdb_port_write_u8 = \
-        "out dx,al" \
-        parm [dx] [al] \
-        modify exact []
-extern void XDB_NEAR xdb_port_write_u16(xdb_u16 port, xdb_u16 value);
-#pragma aux xdb_port_write_u16 = \
-        "out dx,ax" \
-        parm [dx] [ax] \
-        modify exact []
-extern xdb_u8 XDB_NEAR xdb_port_read_u8(xdb_u16 port);
-#pragma aux xdb_port_read_u8 = \
-        "in al,dx" \
-        parm [dx] \
-        value [al] \
-        modify exact []
-extern void XDB_NEAR xdb_port_write_buffer_u8(
-        xdb_u16 port,
-        const volatile xdb_u8 XDB_NEAR *source,
-        xdb_u16 count);
-#pragma aux xdb_port_write_buffer_u8 = \
-        "rep outsb" \
-        parm [dx] [si] [cx] \
-        modify exact [cx si]
+#include <conio.h>
+#define xdb_port_write_u8(port, value) \
+        ((void)outp((port), (value)))
+#define xdb_port_write_u16(port, value) \
+        ((void)outpw((port), (value)))
+#define xdb_port_read_u8(port) ((xdb_u8)inp(port))
+#define xdb_port_write_buffer_u8(port, source, count) \
+    do { \
+        xdb_u16 xdb_port_buffer_index; \
+        for (xdb_port_buffer_index = 0u; \
+             xdb_port_buffer_index != (count); \
+             ++xdb_port_buffer_index) { \
+            (void)outp((port), (source)[xdb_port_buffer_index]); \
+        } \
+    } while (0)
 #elif defined(__TURBOC__) || defined(__BORLANDC__)
+#include <dos.h>
 #define xdb_port_write_u8(port, value) outportb((port), (value))
 #define xdb_port_write_u16(port, value) outport((port), (value))
 #define xdb_port_read_u8(port) inportb(port)
